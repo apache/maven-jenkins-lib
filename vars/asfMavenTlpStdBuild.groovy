af25f0866ae2100d0297185e0fb181128f3da8fb
Stephen Connolly <stephen.alan.connolly@gmail.com>
2017-11-29 14:32:36 +0000

    Ensure the email on failure is always sent if we start the build

--- vars/asfMavenTlpStdBuild.groovy
+++ vars/asfMavenTlpStdBuild.groovy
@@ -1,53 +1,79 @@
+#!/usr/bin/env groovy
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 def call(Map params = [:]) {
-  def oses = params.containsKey('os') ? params.os : ['linux', 'windows']
-  def jdks = params.containsKey('jdks') ? params.jdks : ['7','8','9']
-  def maven = params.containsKey('maven') ? params.maven : '3.x.x'
-  def failFast = params.containsKey('failFast') ? params.failFast : true
-  Map tasks = [failFast: failFast]
-  boolean first = true
-  for (String os in oses) {
-    for (def jdk in jdks) {
-      String label = jenkinsEnv.labelForOS(os);
-      String jdkName = jenkinsEnv.jdkFromVersion(os, "${jdk}")
-      String mvnName = jenkinsEnv.mvnFromVersion(os, "${maven}")
-      echo "OS: ${os} JDK: ${jdk} Maven: ${maven} => Label: ${label} JDK: ${jdkName} Maven: ${mvnName}"
-      if (label == null || jdkName == null || mvnName == null) {
-        echo "Skipping ${os}-jdk${jdk} as unsupported by Jenkins Environment"
-        continue;
-      }
-      String stageId = "${os}-jdk${jdk}"
-      tasks[stageId] = {
-        node(label) {
-          stage("Checkout ${stageId}") {
-            dir('m') {
-              checkout scm
+  try {
+    // set build retention time first
+    def buildRetention
+    if (env.BRANCH_NAME == 'master') {
+      buildRetention = buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '3', daysToKeepStr: '21', numToKeepStr: '25'))
+    } else {
+      buildRetention = buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '1', daysToKeepStr: '7', numToKeepStr: '5'))
+    }
+    properties([buildRetention])
+
+    // now determine the matrix of parallel builds
+    def oses = params.containsKey('os') ? params.os : ['linux', 'windows']
+    def jdks = params.containsKey('jdks') ? params.jdks : ['7','8','9']
+    def maven = params.containsKey('maven') ? params.maven : '3.x.x'
+    def failFast = params.containsKey('failFast') ? params.failFast : true
+    Map tasks = [failFast: failFast]
+    boolean first = true
+    for (String os in oses) {
+      for (def jdk in jdks) {
+        String label = jenkinsEnv.labelForOS(os);
+        String jdkName = jenkinsEnv.jdkFromVersion(os, "${jdk}")
+        String mvnName = jenkinsEnv.mvnFromVersion(os, "${maven}")
+        echo "OS: ${os} JDK: ${jdk} Maven: ${maven} => Label: ${label} JDK: ${jdkName} Maven: ${mvnName}"
+        if (label == null || jdkName == null || mvnName == null) {
+          echo "Skipping ${os}-jdk${jdk} as unsupported by Jenkins Environment"
+          continue;
+        }
+        String stageId = "${os}-jdk${jdk}"
+        tasks[stageId] = {
+          node(label) {
+            stage("Checkout ${stageId}") {
+              dir('m') {
+                checkout scm
+              }
             }
-          }
-          stage("Build ${stageId}") {
-            withMaven(jdk:jdkName, maven:mvnName, mavenLocalRepo:'.repository') {
-              dir ('m') {
-                if (isUnix()) {
-                  sh 'mvn clean verify -Dmaven.test.failure.ignore=true -Dfindbugs.failOnError=false'
-                } else {
-                  bat 'mvn clean verify -Dmaven.test.failure.ignore=true -Dfindbugs.failOnError=false'
+            stage("Build ${stageId}") {
+              withMaven(jdk:jdkName, maven:mvnName, mavenLocalRepo:'.repository') {
+                dir ('m') {
+                  if (isUnix()) {
+                    sh 'mvn clean verify -Dmaven.test.failure.ignore=true -Dfindbugs.failOnError=false'
+                  } else {
+                    bat 'mvn clean verify -Dmaven.test.failure.ignore=true -Dfindbugs.failOnError=false'
+                  }
                 }
               }
             }
           }
         }
+        first = false
       }
-      first = false
     }
-  }
-  def buildRetention
-  if (env.BRANCH_NAME == 'master') {
-    buildRetention = buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '3', daysToKeepStr: '21', numToKeepStr: '25'))
-  } else {
-    buildRetention = buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '1', daysToKeepStr: '7', numToKeepStr: '5'))
-  }
-  properties([buildRetention])
-  try {
+    // run the parallel builds
     parallel(tasks)
+
     // JENKINS-34376 seems to make it hard to detect the aborted builds
   } catch (org.jenkinsci.plugins.workflow.steps.FlowInterruptedException e) {
     // this ambiguous condition means a user probably aborted
@@ -72,6 +98,7 @@ def call(Map params = [:]) {
     currentBuild.result = "FAILURE"
     throw e
   } finally {
+    // notify completion
     stage("Notifications") {
       jenkinsNotify()      
     }    
