f1a8105191b30457c6465bd26edb3ad38546895d
rfscholte <rfscholte@apache.org>
2020-02-22 18:36:02 +0100

    [INFRA-19861] Analyzing...

--- vars/asfMavenTlpPlgnBuild.groovy
+++ vars/asfMavenTlpPlgnBuild.groovy
@@ -129,6 +129,7 @@ def doCreateTask( os, jdk, maven, tasks, first, plan, taskContext )
   if (jdk == '7') {
     // Java 7u80 has TLS 1.2 disabled by default: need to explicitely enable
     cmd += '-Dhttps.protocols=TLSv1.2'
+    cmd += '-X'
   }
 
   if (plan == 'build') {
