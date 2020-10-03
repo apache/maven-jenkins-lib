f502774e9e127d9472021002e812617e018f83a9
rfscholte <rfscholte@apache.org>
2020-10-03 14:04:51 +0200

    pwd() doesn't seem to work on slaves?

--- vars/asfMavenTlpStdBuild.groovy
+++ vars/asfMavenTlpStdBuild.groovy
@@ -76,7 +76,7 @@ def call(Map params = [:]) {
         String stageId = "${os}-jdk${jdk}"
         tasks[stageId] = {
           node("${label}") {
-            def wsDir = pwd()
+            def wsDir = env.WORKSPACE
             if (os == 'windows' && tmpWs) {
               wsDir = 'F:\\short\\' + "$BUILD_TAG".replaceAll(/(.+)_maven-box_maven-(.+)/) { "m-${it[2]}" }
             }
