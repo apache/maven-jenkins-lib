020b09db9a524c1f3355e7e9eddf11420ffc8673
rfscholte <rfscholte@apache.org>
2020-10-18 19:05:13 +0200

    remove debug-flag

--- vars/asfMavenTlpPlgnBuild.groovy
+++ vars/asfMavenTlpPlgnBuild.groovy
@@ -129,7 +129,6 @@ def doCreateTask( os, jdk, maven, tasks, first, plan, taskContext )
   if (jdk == '7') {
     // Java 7u80 has TLS 1.2 disabled by default: need to explicitely enable
     cmd += '-Dhttps.protocols=TLSv1.2'
-    cmd += '-X'
   }
 
   if (plan == 'build') {
