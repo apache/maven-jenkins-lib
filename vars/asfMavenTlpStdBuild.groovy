7774d24db087d27e885b652520532c8ab4ef84ff
Sylwester Lachiewicz <slachiewicz@apache.org>
2021-09-26 17:21:06 +0200

    Drop Java 16 builds

--- vars/asfMavenTlpStdBuild.groovy
+++ vars/asfMavenTlpStdBuild.groovy
@@ -36,7 +36,7 @@ def call(Map params = [:]) {
     // now determine the matrix of parallel builds
     def oses = params.containsKey('os') ? params.os : ['linux', 'windows']
     // minimum, LTS, current and next ea
-    def jdks = params.containsKey('jdks') ? params.jdks : params.containsKey('jdk') ? params.jdk : ['8','11','16','17']
+    def jdks = params.containsKey('jdks') ? params.jdks : params.containsKey('jdk') ? params.jdk : ['8','11','17']
     def maven = params.containsKey('maven') ? params.maven : '3.x.x'
     def tmpWs = params.containsKey('tmpWs') ? params.tmpWs : false
     // def failFast = params.containsKey('failFast') ? params.failFast : true
